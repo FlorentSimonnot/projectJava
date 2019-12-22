d9c1a7819191431e41364f20fbcc7eaf7e901a28
jchu <jchu@etud.u-pem.fr>
2019-12-22 09:32:25 +0100

    simply create a new class for Insn

---
+++ src/main/java/com/project/simpleInstruction/IincInstruction.java
@@ -0,0 +1,30 @@
+package com.project.simpleInstruction;
+
+public class IincInstruction implements Instruction {
+	private final int var;
+	private final int increment;
+	
+	public IincInstruction(int var, int increment) {
+		this.var = var;
+		this.increment = increment;
+	}
+	
+	public int getVar() {
+		return var;
+	}
+	
+	public int getIncrement() {
+		return increment;
+	}
+	
+	@Override
+	void writeInstruction(int version, MethodVisitor mv, Instruction lastInstruction) {
+		mv.visitIincInsn(var, increment);
+	}
+	
+	@Override
+	public String toString() {
+		return "IINC INSN var: " + var + " increment: " + increment;
+	}
+	
+}
